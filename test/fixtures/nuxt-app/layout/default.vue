e33f2b8cefddc56fccba22e62e1cbed6532eac5d
dbarjs <joseeduardobarros@gmail.com>
2022-08-30 15:01:32 -0300

    Create example nuxt-app

---
+++ test/fixtures/nuxt-app/layout/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="MainLayout">
+    <component
+      :is="wrapperTagName"
+      v-if="isWrapperVisible"
+      key="wrapper"
+      data="MainLayout__wrapper"
+      @click="onWrapperClick"
+    >
+      <span>{{ theNumber }}</span>
+    </component>
+
+    <Nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class MainLayout extends Vue {
+  get wrapperTagName(): string {
+    return 'div';
+  }
+
+  get isWrapperVisible(): boolean {
+    return true;
+  }
+
+  get theNumber(): number {
+    return 1;
+  }
+
+  onWrapperClick() {
+    console.log('poiajsdf');
+  }
+}
+</script>
+
+<style lang="scss">
+.Mainlayout {
+  display: flex;
+  background-color: #aaaaaa;
+}
+</style>
